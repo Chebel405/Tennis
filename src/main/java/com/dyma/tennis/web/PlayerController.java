81c88fbd0f1cf983c8ed7724272bc5855831d7d3
Chebel405 <anne.chebel@gmail.com>
2024-04-13 22:43:44 +0200

    Methode Selectionner un joueur par lastName

--- src/main/java/com/dyma/tennis/web/PlayerController.java
+++ src/main/java/com/dyma/tennis/web/PlayerController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -31,5 +32,16 @@ public class PlayerController {
     @GetMapping("/players")
     private List<Player> list(){
         return Collections.emptyList();
+    }
+
+    @Operation(summary = "Finds a player with lastName", description = "Finds player with lastName")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Player",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Player.class))})
+    })
+    @GetMapping("/players/{lastName}")
+    public Player getByLastName(@PathVariable("lastName") String lastName){
+        return null;
     }
 }
