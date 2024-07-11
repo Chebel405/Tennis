65f5c53a3924c923a0b9cf85c3bf4ac83cf1d540
Chebel405 <anne.chebel@gmail.com>
2024-07-11 17:00:48 +0200

    Ajout de l'entité Role et l'entité User + Ajout du dossier Security

--- src/main/docker/init.sql
+++ src/main/docker/init.sql
@@ -1,8 +1,6 @@
--- Créer la séquence seulement si elle n'existe pas déjà
-CREATE SEQUENCE IF NOT EXISTS player_id_seq;
+CREATE SEQUENCE player_id_seq;
 
--- Créer la table seulement si elle n'existe pas déjà
-CREATE TABLE IF NOT EXISTS player
+CREATE TABLE player
 (
     id integer NOT NULL DEFAULT nextval('player_id_seq'),
     last_name character varying(50) NOT NULL,
@@ -13,37 +11,92 @@ CREATE TABLE IF NOT EXISTS player
     PRIMARY KEY (id)
 );
 
--- Assurez-vous que la séquence est liée à la colonne id de la table player
-ALTER SEQUENCE IF EXISTS player_id_seq OWNED BY player.id;
+ALTER SEQUENCE player_id_seq OWNED BY player.id;
 
--- Définir le propriétaire de la table player
-ALTER TABLE IF EXISTS player OWNER TO postgres;
-
--- Insérer des données dans la table player, seulement si elles n'existent pas déjà
-DO
-$$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM player WHERE last_name = 'Nadal' AND first_name = 'Rafael') THEN
-        INSERT INTO player (last_name, first_name, birth_date, points, rank)
-        VALUES ('Nadal', 'Rafael', '1986-06-03', 5000, 1);
-    END IF;
-
-    IF NOT EXISTS (SELECT 1 FROM player WHERE last_name = 'Djokovic' AND first_name = 'Novak') THEN
-        INSERT INTO player (last_name, first_name, birth_date, points, rank)
-        VALUES ('Djokovic', 'Novak', '1987-05-22', 4000, 2);
-    END IF;
-
-    IF NOT EXISTS (SELECT 1 FROM player WHERE last_name = 'Federer' AND first_name = 'Roger') THEN
-        INSERT INTO player (last_name, first_name, birth_date, points, rank)
-        VALUES ('Federer', 'Roger', '1981-08-08', 3000, 3);
-    END IF;
-
-    IF NOT EXISTS (SELECT 1 FROM player WHERE last_name = 'Murray' AND first_name = 'Andy') THEN
-        INSERT INTO player (last_name, first_name, birth_date, points, rank)
-        VALUES ('Murray', 'Andy', '1987-05-15', 2000, 4);
-    END IF;
-
-END
-$$;
+ALTER TABLE IF EXISTS public.player OWNER to postgres;
 
 
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+	VALUES ('Nadal', 'Rafael', '1986-06-03', 5000, 1);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Djokovic', 'Novak', '1987-05-22', 4000, 2);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Federer', 'Roger', '1981-08-08', 3000, 3);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Murray', 'Andy', '1987-05-15', 2000, 4);
+
+CREATE SEQUENCE user_id_seq;
+
+CREATE TABLE dyma_user (
+    id integer NOT NULL DEFAULT nextval('user_id_seq'),
+    login character varying(50) NOT NULL,
+    password character varying(60) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    first_name character varying(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE user_id_seq OWNED BY player.id;
+
+ALTER TABLE IF EXISTS public.dyma_user OWNER to postgres;
+
+CREATE SEQUENCE user_id_seq;
+
+CREATE TABLE dyma_user (
+    id integer NOT NULL DEFAULT nextval('user_id_seq'),
+    login character varying(50) NOT NULL,
+    password character varying(60) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    first_name character varying(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER SEQUENCE user_id_seq OWNED BY player.id;
+
+ALTER TABLE IF EXISTS public.dyma_user OWNER to postgres;
+
+INSERT INTO public.dyma_user(login, password, last_name, first_name)
+	VALUES ('admin', '$2a$12$RkcdJn2kLrAS9fmvDv/CWehqID8nB3XBWXOtazhQ2PY1ZFwDB3L76', 'Dyma', 'Admin');
+
+INSERT INTO public.dyma_user(login, password, last_name, first_name)
+	VALUES ('user', '$2a$12$VRnUGZfeEsWHG9jb7NyvQuhpISK65N2LtWyqXAi5t1CBWIQ34uRNa', 'Doe', 'John');
+
+
+CREATE TABLE dyma_role
+(
+    name character varying(50) NOT NULL,
+    PRIMARY KEY (name)
+);
+
+INSERT INTO public.dyma_role(name)
+	VALUES ('ROLE_ADMIN');
+
+INSERT INTO public.dyma_role(name)
+	VALUES ('ROLE_USER');
+
+ALTER TABLE IF EXISTS public.dyma_role OWNER to postgres;
+
+CREATE TABLE dyma_user_role
+(
+    user_id bigint NOT NULL,
+    role_name character varying(50) NOT NULL,
+    CONSTRAINT dyma_user_role_pkey PRIMARY KEY (user_id, role_name),
+    CONSTRAINT fk_role_name FOREIGN KEY (role_name)
+        REFERENCES public.dyma_role (name),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id)
+        REFERENCES public.dyma_user (id)
+);
+
+ALTER TABLE IF EXISTS public.dyma_user_role OWNER to postgres;
+
+INSERT INTO public.dyma_user_role(user_id, role_name)
+	VALUES (1, 'ROLE_ADMIN');
+
+INSERT INTO public.dyma_user_role(user_id, role_name)
+	VALUES (1, 'ROLE_USER');
+
+INSERT INTO public.dyma_user_role(user_id, role_name)
+	VALUES (2, 'ROLE_USER');
