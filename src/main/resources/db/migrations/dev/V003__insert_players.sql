4e80accda656ca7fa3a8c8fe5cda9e8790a172b5
Chebel405 <anne.chebel@gmail.com>
2025-01-20 19:24:22 +0100

    Erreur au niveau des tests et de l'activation du projet

---
+++ src/main/resources/db/migrations/dev/V003__insert_players.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+	VALUES ('Nadal', 'Rafael', '1986-06-03', 5000, 1);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Djokovic', 'Novak', '1987-05-22', 4000, 2);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Federer', 'Roger', '1981-08-08', 3000, 3);
+
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank)
+    	VALUES ('Murray', 'Andy', '1987-05-15', 2000, 4);
---
+++ src/main/resources/db/migrations/prod/V003__insert_players.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.player(last_name, first_name, birth_date, points, rank) VALUES
+('Djokovic', 'Novak', '1987-05-22', 9855, 1),
+('Alcaraz', 'Carlos', '2003-05-05', 8805, 2),
+('Jannik', 'Sinner', '2001-08-16', 8270, 3),
+('Daniil', 'Medvedev', '1996-02-11', 8015, 4),
+('Andrey', 'Rublev', '1997-10-20', 5110, 5),
+('Alexander', 'Zverev', '1997-04-20', 5085, 6),
+('Holger', 'Rune', '2003-04-29', 3700, 7),
+('Hubert', 'Hurkacz', '1997-02-11', 3395, 8),
+('Alex', 'de Minaur', '1999-02-17', 3210, 9),
+('Taylor', 'Fritz', '1997-10-28', 3150, 10);
